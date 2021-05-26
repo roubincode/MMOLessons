05b7cb90c72c07157fcc73231ff5e26910d64f48
roubing <liaoxiangning@taikr.com>
2021-05-26 22:58:39 +0800

    聊天基本逻辑

--- Unity/Assets/MMORPG/Script/RPGGame/UIPanel/MapUIFramePanel.cs
+++ Unity/Assets/MMORPG/Script/RPGGame/UIPanel/MapUIFramePanel.cs
@@ -5,8 +5,16 @@ using UnityEngine.UI;
 public class MapUIFramePanel : UIPanel
 {
     public Button btn_setting;
+    public GameObject chatContent;
+    public UIMessageSlot messageSlot;
     void Start()
     {
+        // 世界频道通知
+        GameObject message = RPGManager.Instance.CreateItem(messageSlot.gameObject);
+        message.transform.SetParent(chatContent.transform);
+        UIMessageSlot slot = message.GetComponent<UIMessageSlot>();
+        slot.text.text = "系统：欢迎来到MMORPG游戏的黎明镇！\n愿你有一个美好的游戏体验，快乐游戏，文明游戏。";
+
         btn_setting.onClick.SetListener(() => {
             uIState.EnterSettingPanel();
         });
