54856ac6020eee614931f7c9a498c6e6e4ec428b
roubing <liaoxiangning@taikr.com>
2021-05-31 19:59:25 +0800

    补一章练习

---
+++ Exercises/Chapter1/MainModule.cs
@@ -0,0 +1,9 @@
+using System;
+class MainModule
+{
+    static void Main()
+    {
+        MyFirstModule.Hello();
+        MySecondModule.Hello();
+    }
+}
---
+++ Exercises/Chapter1/MyClass.cs
@@ -0,0 +1,8 @@
+using System;
+public class MyClass
+{
+    public static void PrintSth()
+    {
+        Console.WriteLine("Hello");
+    }
+}
