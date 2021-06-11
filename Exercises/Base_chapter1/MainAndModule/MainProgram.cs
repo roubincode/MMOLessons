e858d3de47933350c3f48d52ec552e5e51366312
roubing <liaoxiangning@taikr.com>
2021-06-11 20:14:51 +0800

    修改MyILClass.il

--- Exercises/Base_chapter1/MainAndModule/MainProgram.cs
+++ Exercises/Base_chapter1/MainAndModule/MainProgram.cs
@@ -4,9 +4,9 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyVersion("1.2.0.0")]
 class MainProgram
 {
-    
     static void Main()
     {
         MyClass.PrintSth();
+        MyILClass.PrintSth();
     }
 }
